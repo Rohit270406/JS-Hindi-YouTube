ccf1406603106ba82d3ff2b0dd9f22f6fb4026d9
Rohit270406 <royrohitofficial67904@gmail.com>
2026-05-15 13:04:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="Royrohit2@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+//accountId=2;
+accountEmail="royrohit34@gmail.com"
+accountPassword="270406"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*prefer not to use var
+because of issue in block scope and functional scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using node.js, not browser
+console.log(3+3)
+console.log("Rohit")
+let name="RoyRohit"
+let age=20
+let isLoggedIn=false;
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value;
+//undefined=>
+    //symbol=>unique;
+//object
+console.log(typeof"rohit");
