4a84a6c9f5ee66a7d25040e7140e834ef7aa5153
Rohit Roy <royrohitofficial67904@gmail.com>
2026-05-30 19:07:29 +0000

    functions part

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,7 +23,28 @@ function loginUserMessage(username="robi"){
         console.log("Please enter a  username");
         return;
     }
-    return `${username} just logged in`
+    return `${username} just logged in`;
 }
 //console.log(loginUserMessage("Rohit"))
 console.log(loginUserMessage())
+function calculateCartPrice(val1,val2,...num1){
+return num1;
+}
+console.log(calculateCartPrice(200,400,500))
+const user={
+    username: "rohit",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username:"ROhit",
+    price:299
+})
+const myNewArray=[200,400,100,600]
+function  returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
