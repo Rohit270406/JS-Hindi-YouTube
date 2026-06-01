6868af4872d9f9d5ba2d1b587c7d3db6d359adfb
Rohit Roy <royrohitofficial67904@gmail.com>
2026-06-01 20:51:30 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,30 @@ if(true){
 }
 
 console.log(a)
+
+function one(){
+    const username="rohit"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    if(true){
+        const username="rohit"
+        if(username==="rohit"){
+            const website="youtube"
+        }
+    }
+   //
+   // console.log(website);
+    two()
+}
+one()
+//+++++++++++++++++++++++++++++++++intersting+++++++++
+function addone(num){
+    return num+1;
+}
+console.log(addone(5))
+ const addTwo=function(num){
+    return  num+2;
+ }
+ addTwo(5)
