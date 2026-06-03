87d651f141798afd44bef7dd3dbbcf0c68f29126
Rohit Roy <royrohitofficial67904@gmail.com>
2026-06-03 19:49:37 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,8 @@
+(function chai (){
+    //name iife
+    console.log(`DB CONNECTED`);
+})();
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('rohit');
+//IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
