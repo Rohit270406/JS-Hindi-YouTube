3f8ab0f871e51e888035ae0fbcea114c8e1a15ba
Rohit Roy <royrohitofficial67904@gmail.com>
2026-06-26 19:04:02 +0000

    loop concept

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+//for
+for(let index=0;index<10;index++){
+    const element=index;
+    console.log(element);
+}
+let myArray=["flash","batman","superman"]
+for(let index=0;index<myArray.length;index++){
+    const element=myArray[index];
+    console.log(element);
+}
